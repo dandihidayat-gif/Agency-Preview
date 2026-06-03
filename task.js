e004e9714b6096dc29beb84c5a4bc252066d9809
dandihidayat-gif <dandihidayat1@gmail.com>
2026-06-03 20:49:12 +0700

    Update task.js

--- task.js
+++ task.js
@@ -6,6 +6,10 @@ const todoView = document.getElementById("todoView");
 
 if(todoBtn){
   todoBtn.addEventListener("click", () => {
+
+    plannerBtn.classList.remove("active");
+    todoBtn.classList.add("active");
+
     document.querySelector(".main-content").style.display = "none";
     todoView.style.display = "block";
   });
@@ -13,6 +17,10 @@ if(todoBtn){
 
 if(plannerBtn){
   plannerBtn.addEventListener("click", () => {
+
+    todoBtn.classList.remove("active");
+    plannerBtn.classList.add("active");
+
     todoView.style.display = "none";
     document.querySelector(".main-content").style.display = "block";
   });
